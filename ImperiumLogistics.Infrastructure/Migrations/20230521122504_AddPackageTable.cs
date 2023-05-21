7e49fb6a7f339751c9dadc0b6354d18fafb48128
Emmanuel Mgbemena <Emmanuel.Mgbemena@wemabank.com>
2023-05-21 13:26:12 +0100

    Ran migration to create package table

---
+++ ImperiumLogistics.Infrastructure/Migrations/20230521122504_AddPackageTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImperiumLogistics.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPackageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Package",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlacedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DeliveryAddress_Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DeliveryAddress_City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DeliveryAddress_State = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DeliveryAddress_LandMark = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    PickUpAddress_Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PickUpAddress_City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PickUpAddress_State = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PickUpAddress_LandMark = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Cusomer_FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cusomer_LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cusomer_PhoneNumber = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastDateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)),
+                    TrackingNumber = table.Column<string>(type: "nvarchar(28)", maxLength: 28, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Package", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackageDescription",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DateUpdated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackageDescription", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Package");
+
+            migrationBuilder.DropTable(
+                name: "PackageDescription");
+        }
+    }
+}
