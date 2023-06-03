8d28b9ec2c371725a4c53fd36b492c365258b89d
Emmanuel Mgbemena <Emmanuel.Mgbemena@wemabank.com>
2023-06-03 22:51:52 +0100

    Ran migration to add QRCode to Package entity

---
+++ ImperiumLogistics.Infrastructure/Migrations/20230603215056_AddedQRCodeToPackage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImperiumLogistics.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedQRCodeToPackage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QRCode",
+                table: "Package",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QRCode",
+                table: "Package");
+        }
+    }
+}
